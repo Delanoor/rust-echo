b42417494cf880d34fdded469bb8b1e0b1d2a053
hwn0809 <hwn0809@gmail.com>
2024-02-17 14:56:53 +0900

    update Katoo

--- bird/src/main.rs
+++ bird/src/main.rs
@@ -1,30 +1,36 @@
+use std::str::FromStr;
+
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
 };
+use uuid::Uuid;
 
-const SIROCCO_SERVER_ADDRESS: &str = "127.0.0.1:8000";
+const KATOO_SERVER_ADDRESS: &str = "127.0.0.1:8000";
 const SERVER_ADDRESS: &str = "127.0.0.1:8001";
 
 #[tokio::main]
 async fn main() {
-    println!("Karin starting {}", SERVER_ADDRESS);
+    println!("Bird starting {}", SERVER_ADDRESS);
 
     // bind
     let listener = TcpListener::bind(SERVER_ADDRESS).await.unwrap();
 
-    println!("karin listening {}", SERVER_ADDRESS);
+    println!("Bird listening {}", SERVER_ADDRESS);
 
     loop {
-        let (stream, _) = listener.accept().await.unwrap();
+        let (socket, _) = listener.accept().await.unwrap();
 
         tokio::spawn(async move {
-            handle_connection(stream).await;
+            handle_connection(socket).await;
         });
     }
 }
 
 async fn handle_connection(mut stream: TcpStream) {
+    // create uuid
+    let id = Uuid::new_v4();
+
     // read the buffer
     let mut buffer = [0; 1024];
     let len = stream.read(&mut buffer).await.unwrap();
@@ -32,42 +38,40 @@ async fn handle_connection(mut stream: TcpStream) {
 
     println!("received: {}", message);
 
-    // call sirocco
-    let sirocco_message = call_sirocco(message.to_string()).await;
-    let output = format!("sirocco says: {}", sirocco_message);
+    // call katoo
+    let katoo_message = call_katoo(id, message.to_string()).await;
+    let output = format!("Katoo says: {}", katoo_message);
 
-    // write the message
+    // send out message
     let _ = stream.write_all(output.as_bytes()).await;
-    println!("sent: {}", output);
+    println!("{} - sent: {}", id, message);
 }
 
-async fn call_sirocco(message: String) -> String {
+async fn call_katoo(id: Uuid, message: String) -> String {
     // connection message
-    println!("connecting to sirocco {}", SIROCCO_SERVER_ADDRESS);
+    println!("{} - connecting to Katoo {}", id, KATOO_SERVER_ADDRESS);
 
-    if let Ok(mut stream) = TcpStream::connect(SIROCCO_SERVER_ADDRESS).await {
+    if let Ok(mut stream) = TcpStream::connect(KATOO_SERVER_ADDRESS).await {
         // connected
         println!(
-            "connected to sirocco {}:{}",
+            "{} - connected to Katoo {}:{}",
+            id,
             stream.local_addr().unwrap().ip(),
             stream.local_addr().unwrap().port()
         );
 
         let _ = stream.write_all(message.as_bytes()).await;
-        println!("sent {} to Sirocco", message);
+        println!("{} - sent {}", id, message);
 
         // read the result
         let mut buff = [0; 1024];
         let _len = stream.read(&mut buff).await.unwrap();
         let message = String::from_utf8_lossy(&buff);
-        println!("received from sirocco: {}", message);
+        println!("{} - received from Katoo: {}", id, message);
 
         message.to_owned().to_string()
     } else {
-        println!(
-            "failed to connect to sirocco server {}",
-            SIROCCO_SERVER_ADDRESS
-        );
-        "Failed".to_owned()
+        println!("failed to connect to Katoo server {}", KATOO_SERVER_ADDRESS);
+        String::from_str("Katoo not available").unwrap()
     }
 }
